e18ffe30a067e653eba03e5b2f565206a769305e
playatlas <atlasafri@gmail.com>
2023-03-13 07:51:38 +0300

    added reatlaz.pythonanywhere.com to allowed hosts

--- application/settings.py
+++ application/settings.py
@@ -37,7 +37,9 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'reatlaz.pythonanywhere.com'
+]
 
 
 # Application definition
